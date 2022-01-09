b6509665409d028a3aa73c2fd7c0455ebfaacdd7
Tomek Mroziński <mrozoo@icloud.com>
2022-01-09 11:16:39 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tomek on 09/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
