571e83b2b25b63dd2fadc40db7042ef8e019584a
Tomek Mroziński <mrozoo@icloud.com>
2022-01-09 20:32:43 +0100

    Project 1, part one

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount: Int = 0
+    @State private var name: String = ""
+    
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Button("Tap Count: \(tapCount)") {
+                        tapCount += 1
+                    }
+                    TextField("Enter your name", text: $name)
+                    Text("Your name is \(name)")
+                }
+                Section {
+                    Picker("Select your student", selection: $selectedStudent) {
+                        ForEach(students, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                Group {
+                    Text("Hello, world!")
+                }
+                Group {
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
+                Section {
+                    Text("Hello, world!")
+                }
+                Section {
+                    Text("Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
