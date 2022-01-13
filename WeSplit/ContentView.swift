5d49bdc47a8a3fdea9221d64fef2a9fa1f433ebe
Tomek Mroziński <mrozoo@icloud.com>
2022-01-13 22:46:55 +0100

    Dodanie pickerów

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    let currencyCode = Locale.current.currencyCode ?? "USD"
     
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) { number in
+                            Text("\(number)")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: currencyCode))
+                    Text("Selected \(numberOfPeople) people")
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
