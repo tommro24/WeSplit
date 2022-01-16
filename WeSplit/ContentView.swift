18e4e431a656d6d2f2f0ff207c0874466d8036aa
Tomek Mroziński <mrozoo@icloud.com>
2022-01-16 22:00:44 +0100

    Wyliczenie rachunku przypadającego na osobę

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,10 +11,15 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10,15,20,25,0]
     
     let currencyCode = Locale.current.currencyCode ?? "USD"
+    
+    var totalPerPerson: Double {
+        return (checkAmount * (1 + Double(tipPercentage)/100)) / Double(numberOfPeople + 2)
+    }
     
     var body: some View {
         NavigationView {
@@ -22,6 +27,7 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) { number in
@@ -43,11 +49,20 @@ struct ContentView: View {
                 
                 
                 Section {
-                    Text(checkAmount, format: .currency(code: currencyCode))
-                    Text("Selected \(numberOfPeople) people")
+                    Text(totalPerPerson, format: .currency(code: currencyCode))
+                } header: {
+                    Text("Total per person")
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
